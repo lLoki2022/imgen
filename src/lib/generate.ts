241cf042177df1444bea8471de7599434df2b38c
lLoki2022 <110937330+lLoki2022@users.noreply.github.com>
2026-05-19 13:40:57 +0300

    Update generate.ts

--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -3,22 +3,12 @@ import img from './create/img';
 import txt from './create/txt';
 import shp from './create/shp';
 import { hex } from './helpers.js';
-
-export async function generateImage(ctx: Ctx, config: Config) {
-    if (config.fill) {
-        ctx.fillStyle = hex(config.fill);
-        ctx.fillRect(0, 0, config.width, config.height);
-    }
-
-    const create = { config, ctx, txt, img, shp };
-
+export async function generateImage(ctx: Ctx, config: Config, frameIndex: number = 0) {
+    if (config.fill) { ctx.fillStyle = hex(config.fill); ctx.fillRect(0,0,config.width,config.height); }
+    else { ctx.clearRect(0,0,config.width,config.height); }
+    const create = { config, ctx, frameIndex, txt, img, shp };
     for (const cfg of config.layers) {
-        try {
-            await create[cfg.type](cfg)
-        } catch (err) {
-            console.log(err);
-        }
+        try { await create[cfg.type].call(create, cfg) } catch (err) { console.log(err); }
     }
-
     return ctx.canvas;
 }
