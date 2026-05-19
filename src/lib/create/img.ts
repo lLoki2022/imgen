71c56205644f73c2b35519959534e81bd40c52bf
lLoki2022 <110937330+lLoki2022@users.noreply.github.com>
2026-05-19 13:41:31 +0300

    Update img.ts

--- src/lib/create/img.ts
+++ src/lib/create/img.ts
@@ -1,71 +1,34 @@
 import type { ItemThis, LayerShp } from "$lib/types";
-import { CreateStroke, hex, parseOrigin, toNum } from "../helpers";
-
-const origin = (v: number) => ({
-    s: 0,
-    c: -v / 2 ,
-    e: -v ,
-    t: 0,
-    m: -v / 2,
-    b: -v ,
-})
+import { CreateStroke, getRound, hex, parseOrigin, toNum } from "../helpers";
+const origin = (v:number) => ({ s:0, c:-v/2, e:-v, t:0, m:-v/2, b:-v })
 
 export default async function(this: ItemThis, p: LayerShp) {
+    p.x = toNum(p.x) ?? 0; p.y = toNum(p.y) ?? 0;
     p.w = toNum(p.w) ?? this.config.width;
     p.h = toNum(p.h) ?? this.config.height;
-    p.x = toNum(p.x) ?? this.config.height / 2;
-    p.y = toNum(p.y) ?? this.config.height / 2;
-
     this.ctx.save();
-    
-
     let [ox, oy] = parseOrigin(p.o || 'st'); 
-
-    let x = origin(p.w)[ox];
-    let y = origin(p.h)[oy];
-
-
+    let x = origin(p.w)[ox], y = origin(p.h)[oy];
     this.ctx.translate(p.x, p.y);
     this.ctx.rotate(Math.PI / (180 / (toNum(p.r) || 0)));
-
-
     CreateStroke(this.ctx, p, x, y)
-
-    
-    
-
     let region = new Path2D();
-    region.roundRect(x, y, p.w, p.h, toNum(p.rd) || 0);
+    region.roundRect(x, y, p.w, p.h, getRound(p));
     this.ctx.clip(region, 'evenodd');
-
-
-    // console.log(p.data, "server");
-    
-    
     if (typeof p.data == "string") {
         const dec = decodeURIComponent(p.data);
-        p.data = {
-            // @ts-ignore
-            $elem: await loadImage(dec),
-            // @ts-ignore
-            $name: dec,
-        };
+        // @ts-ignore
+        p.data = { $elem: await loadImage(dec), $name: dec };
     }
-
-    
-
     // @ts-ignore
-    this.ctx.drawImage(
-        p.data.$elem,
-        x, y,
-        p.w,
-        p.h
-    );
-
+    const frames: any[] | undefined = p.data?.$frames;
+    let source: any = p.data.$elem;
+    if (frames && frames.length > 0) {
+        const idx = (this.frameIndex ?? 0) % frames.length;
+        const f = frames[idx];
+        source = f.canvas || f.$elem || source;
+    }
+    // @ts-ignore
+    this.ctx.drawImage(source, x, y, p.w, p.h);
     this.ctx.restore();
 }
-
-
-
-
-
